36a2a05f267f33e6df1e3fec147920aa47f17828
Ivan Yelizariev <yelizariev@it-projects.info>
2018-06-08 19:39:25 +0500

    [NEW] web_website
    [DEV] add doc

---
+++ web_website/__manifest__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Ivan Yelizariev <https://it-projects.info/team/yelizariev>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+{
+    "name": """Website Switcher in Backend""",
+    "summary": """Technical module to switch Websites in Backend similarly to Company Switcher""",
+    "category": "Hidden",
+    # "live_test_url": "",
+    "images": [],
+    "version": "10.0.1.0.0",
+    "application": False,
+
+    "author": "IT-Projects LLC, Ivan Yelizariev",
+    "support": "apps@it-projects.info",
+    "website": "https://it-projects.info/team/yelizariev",
+    "license": "LGPL-3",
+    # "price": 9.00,
+    # "currency": "EUR",
+
+    "depends": [
+        "web",
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
+    ],
+    "demo": [
+    ],
+    "qweb": [
+    ],
+
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+    "uninstall_hook": None,
+
+    "auto_install": False,
+    "installable": True,
+}
---
+++ web_website/doc/index.rst
@@ -0,0 +1,22 @@
+=============================
+ Website Switcher in Backend
+=============================
+
+Installation
+============
+
+* Add this module to server-wide modules, for example::
+
+      ./odoo-bin --load=web,web_website
+* `Install <https://odoo-development.readthedocs.io/en/latest/odoo/usage/install-module.html>`__ this module in a usual way
+* As this is a technical module, consider to install other modules that use this one, for example `ir_config_parameter_multi_company <https://apps.odoo.com/apps/modules/10.0/ir_config_parameter_multi_company/>`_
+
+Usage
+=====
+
+After installation you will see Website Switcher in top right-hand corner
+
+Uninstallation
+==============
+
+Don't forget to remove ``web_website`` from server-wide modules
